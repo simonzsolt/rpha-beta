f76a5fe126e8487022d5448947b9231b205c3da8
Simon Zsolt <simon.zsolt@mail.com>
2017-01-10 20:09:59 +0100

    basic routing node running configs TODO: view, DB connection

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var app = express();
+var path = require('path');
+var config = require('config');
+var MongoClient = require('mongodb').MongoClient
+var assert = require('assert');
+
+var index = require('./routes/index');
+
+// var morgan = require('morgan');
+
+// app.use(morgan('combined'));
+
+var MONGO_DB_URI =  config.get('MONGO_DB_URI');
+
+MongoClient.connect(MONGO_DB_URI, function (err, db) {
+     assert.equal(null, err);
+     console.log("Connected correctly to mongodb server");
+
+     db.close();
+});
+
+var port = config.get('port') || 3000;
+var host = config.get('host') || 'localhost';
+
+app.listen(port, function () {
+    console.log('Example app listening on ' + host + ':' + port + ', in ' + process.env.NODE_ENV + ' environment.')
+})
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/',  index)
+
+module.exports = app;
