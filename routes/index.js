e3c97bcb3b7cb671a333197ea8cb0f1ef781be56
Simon Zsolt <simon.zsolt@mail.com>
2017-01-18 18:27:06 +0100

    simple html view; reload module

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,4 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res){
-        res.send("2 files");
-});
-
 module.exports = router
--- server.js
+++ server.js
@@ -1,36 +1,41 @@
 var express = require('express');
 var router = express.Router();
 var app = express();
+var http = require('http');
 var path = require('path');
 var config = require('config');
 var MongoClient = require('mongodb').MongoClient
 var assert = require('assert');
 
+// routes
 var index = require('./routes/index');
 
-// var morgan = require('morgan');
+var morgan = require('morgan');
+app.use(morgan('combined'));
 
-// app.use(morgan('combined'));
+// DB connection
+var MONGO_DB_URI = config.get('MONGO_DB_URI');
+MongoClient.connect(MONGO_DB_URI, function(err, db) {
+    assert.equal(null, err);
+    console.log('[server.js] Connected correctly to mongodb server.');
 
-var MONGO_DB_URI =  config.get('MONGO_DB_URI');
-
-MongoClient.connect(MONGO_DB_URI, function (err, db) {
-     assert.equal(null, err);
-     console.log("[server.js] Connected correctly to mongodb server.");
-
-     db.close();
+    db.close();
 });
 
+// server connection config
 var port = config.get('port') || 3000;
 var host = config.get('host') || 'localhost';
 
-app.listen(port, function () {
+app.listen(port, function() {
     console.log('[server.js] Example app listening on "http://' + host + ':' + port + '", in ' +
         process.env.NODE_ENV + '.' + ' environment.')
 })
 
-// app.use(express.static(path.join(__dirname, 'public')));
+// path to public folder
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
-app.use('/',  index)
+// use index route
+app.use('/', index)
 
 module.exports = app;
