68d7e073b7e966e01a320091f32f9fd21399f464
Simon Zsolt <simon.zsolt@mail.com>
2017-02-12 18:51:24 +0100

    removing mongodb connection;

--- routes/index.js
+++ routes/index.js
@@ -1,84 +1,8 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
-var mongoose = require('mongoose');
-var Verse = mongoose.model('Verse', Verse);
-var Source = mongoose.model('Source', Source);
 var winston = require('winston');
 
-router.get('/', function(req, res) {
-    res.render('./views/index.html', function(err, html) {
-        res.send(html)
-    })
-});
 
-router.get('/verse', function(req, res, next) {
-    Verse.find({}).exec()
-        .then(function(verse) {
-            res.json(verse);
-        })
-        .catch(function(err) {
-            winston.error([index.js] + "\t" + err);
-        });
-});
-
-router.get('/verse/:id', function(req, res, next) {
-    Verse.find({
-            "id": req.params.id
-        }).exec()
-        .then(function(verse) {
-            res.json(verse);
-        })
-        .catch(function(err) {
-            winston.error('[index.js] \t' + err);
-        })
-});
-
-router.get('/source', function(req, res, next) {
-    Source.find({}).exec()
-        .then(function(source) {
-            res.json(source);
-        })
-        .catch(function(err) {
-            winston.error([index.js] + "\t" + err);
-        });
-});
-
-router.get('/source/:id', function(req, res, next) {
-    Source.find({
-            "id": req.params.id
-        }).exec()
-        .then(function(source) {
-            res.json(source);
-        })
-        .catch(function(err) {
-            winston.error([index.js] + "\t" + err);
-        })
-});
-
-router.get('/:id', function(req, res, next) {
-    Verse.aggregate([
-        {
-            $match: { "id":req.params.id }
-        },
-        { $unwind: "$source" },
-        {
-            $lookup:{
-                from: "source",
-                localField: "source.bookId",
-                foreignField: "id",
-                as: "books"
-            }
-        }
-    ]).exec()
-    .then(function(total) {
-        res.json(total);
-    })
-    .catch(next);
-});
-
-app.use(function (err, req, res, next) {
-    winston.error('[index.js] \t' + err);
-})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,10 +5,7 @@ var app = express();
 var http = require('http');
 var path = require('path');
 var config = require('config');
-// var morgan = require('morgan');
 var winston = require('winston');
-var mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
 
 
 // logging
@@ -27,9 +24,6 @@ winston.configure({
     ]
 });
 
-// require model
-var Verse = require('./public/models/verse').Verse;
-var Source = require('./public/models/source').Source;
 
 // routes
 var index = require('./routes/index');
@@ -38,14 +32,7 @@ var index = require('./routes/index');
 app.use(compression())
 
 // DB connection
-var MONGO_DB_URI = config.get('MONGO_DB_URI');
-mongoose.connect(MONGO_DB_URI);
-var db = mongoose.connection;
-// on connection error
-db.on('error', winston.error.bind(winston, 'connection error:'));
-db.once('open', function() {
-    winston.info('[server.js] Connected correctly to mongodb server.');
-});
+
 
 // server connection config
 var port = config.get('port') || 3000;
